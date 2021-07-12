5c3f67dd68483866854cc5674cb780aaa62571bf
taffil <77844556+taffil@users.noreply.github.com>
2021-07-12 23:28:31 +0200

    navbar

--- masterContent/masterJS.js
+++ masterContent/masterJS.js
@@ -3,10 +3,8 @@ const navbar = document.querySelector("#nav");
 const logbar = document.querySelector("#log");
 
 button.addEventListener("click", function() {
-    // Toggle class "is-active"
     navbar.classList.toggle("passive")
     logbar.classList.toggle("passive")
-        // logbar.style.display = "flex"
 });
 
 // function myFunction() {
