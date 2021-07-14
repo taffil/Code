51b945d8fac07574f9d14cc06ea4fb8e9923e862
taffil <77844556+taffil@users.noreply.github.com>
2021-07-14 17:36:27 +0200

    cheat sheet added

--- masterContent/masterJS.js
+++ masterContent/masterJS.js
@@ -7,13 +7,50 @@ button.addEventListener("click", function() {
     logbar.classList.toggle("passive")
 });
 
-// function myFunction() {
-//     var x = document.getElementById("button");
-//     if (navbar.style.display === "") {
-//         x.style.display = "flex";
-//         // x.classList.append(".felx-center-evenly")
-//     } else {
-//         x.style.display = "";
-//         // x.classList.remove(".felx-center-evenly")
-//     }
-// }
+function has() {
+    window.location.href = "https://css-tricks.com/meet-has-a-native-css-parent-selector/";
+}
+
+function svg() {
+    window.location.href = "https://css-tricks.com/svg-favicons-in-action/";
+}
+
+function where() {
+    window.location.href = "https://css-tricks.com/using-the-specificity-of-where-as-a-css-reset/";
+}
+
+function conic() {
+    window.location.href = "https://css-tricks.com/my-struggle-to-use-and-animate-a-conic-gradient-in-svg/";
+}
+
+function imageSet() {
+    window.location.href = "https://css-tricks.com/using-performant-next-gen-images-in-css-with-image-set/";
+}
+
+function toggle() {
+    window.location.href = "https://css-tricks.com/body-toggle/";
+}
+
+function py() {
+    window.location.href = "https://www.pythoncheatsheet.org/";
+}
+
+function csharp() {
+    window.location.href = "https://hackr.io/blog/c-sharp-cheat-sheet";
+}
+
+function htmlcss() {
+    window.location.href = "https://ilovecoding.org/blog/htmlcss-cheatsheet";
+}
+
+function react() {
+    window.location.href = "https://www.freecodecamp.org/news/react-cheatsheet-with-real-world-examples/";
+}
+
+function js() {
+    window.location.href = "https://htmlcheatsheet.com/js/";
+}
+
+function java() {
+    window.location.href = "https://introcs.cs.princeton.edu/java/11cheatsheet/";
+}
